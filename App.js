8949ffb368f377cdf835869156889e0f494aaa7d
sudarma02 <37562724+sudarma02@users.noreply.github.com>
2018-03-26 23:42:59 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, AppRegistry,Button,TextInput } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, Button, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+export default class Cuaca extends React.Component {
 
   constructor(props) {
-      super(props);
+      super(props)
       this.state = {
         city: '',
         forecast: {
           main: '-',
           description: '-',
-          temp: 0,
-          sunrise: '-',
-          sunset: '-',
-          country: '-',
-          speed: '-',
-          sea_level: '-',
+          temp: '-',
           pressure: '-',
-          humidity: '-'
+          humidity: '-',
+          sea_level: '-',
+          wind_speed: '-',
+          sunrise: '-',
+          sunset: '-'
         }
       };
     }
 
-    getWeather= () => {
-    let url = 'http://api.openweathermap.org/data/2.5/weather?q='+this.state.city+ '&appid=7fb00d5313a4577c055411e5ea605746&units=metric';
+    getWeather= () =>{
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.state.city + '&appid=547e623a913f1fd7c15224590bcf2bfd&units=metric';
     fetch (url)
     .then((response) => response.json())
     .then((responseJson) => {
@@ -33,197 +32,171 @@ export default class App extends React.Component {
           main: responseJson.weather[0].main,
           description: responseJson.weather[0].description,
           temp: responseJson.main.temp,
-          sunrise: responseJson.sys.sunrise,
-          sunset: responseJson.sys.sunset,
-          country: responseJson.sys.country,
-          speed: responseJson.wind.speed,
-          sea_level: responseJson.main.sea_level,
-          grnd_level: responseJson.main.grnd_level,
           pressure: responseJson.main.pressure,
-          humidity: responseJson.main.humidity
+          humidity: responseJson.main.humidity,
+          sea_level: responseJson.main.sea_level,
+          wind_speed: responseJson.wind.speed,
+          sunrise: responseJson.sys.sunrise,
+          sunset: responseJson.sys.sunset
         }
       });
     });
   }
-
     render() {
       return (
         <View style={styles.containerMain}>
-          <View style={styles.header}>
-            <Text style={styles.text1}>Prakiraan Cuaca</Text>
+          <View style={styles.box1}>
+            <Text style={styles.text1}>Prakiraan Cuaca Sederhana</Text>
           </View>
-          <View style={styles.inputtext}>
-              <TextInput style = {styles.text2}
-                placeholder="Masukkan Nama Kota"
-                onChangeText={(city)=>this.setState({city})}
-              />
-
-              <Button
-                onPress={
-                  () => this.getWeather()
-                }
-                title="Lihat"
-                color="#00B0FF"
-                accessibilityLabel="Klik untuk melihat"
-              />
-          </View>
-
-          <View style={styles.infocuaca}>
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Kota = {this.state.city} {"\n"}
-                </Text>
+          <View style={styles.box2}>
+                <TextInput
+                  style={styles.text1}
+                  placeholder="Masukkan Nama Kota"
+                  onChangeText={(city) => this.setState({ city })}
+                />
+                <Button
+                onPress={() => this.getWeather()}
+                  title="Lihat"
+                  color="#2E7D32"
+                  accessibilityLabel="Klik untuk melihat"
+                />
               </View>
+
+              <View style={styles.box3}>
+
+                <View style={styles.box4}>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2}>
+                      Cuaca = {this.state.forecast.main} {'\n'}
+                    </Text>
+                  </View>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2}>
+                      Deskripsi = {this.state.forecast.description} {'\n'}
+                    </Text>
+                  </View>
+                </View>
+
+                <View style={styles.box4}>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2} >
+                      Temperatur = {this.state.forecast.temp} {"'Celcius"}
+                    </Text>
+                  </View>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2} >
+                      Tekanan = {this.state.forecast.pressure} {'\n'}
+                    </Text>
+                  </View>
+                </View>
+
+                <View style={styles.box4}>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2} >
+                      Kelembapan = {this.state.forecast.humidity} {'\n'}
+                    </Text>
+                  </View>
+                  <View style={styles.box5}>
+                    <Text style={styles.text2} >
+                      Tinggi air laut = {this.state.forecast.sea_level} {'\n'}
+                    </Text>
+                  </View>
+                </View>
+
               <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Negara = {this.state.forecast.country} {"\n"}
-                </Text>
+                <View style={styles.box5}>
+                  <Text style={styles.text2} >
+                    Waktu Terbit Matahari = {this.state.forecast.sunrise}
+                  </Text>
+                </View>
+                <View style={styles.box5}>
+                  <Text style={styles.text2} >
+                    Waktu Terbenam Matahari = {this.state.forecast.sunset}
+                  </Text>
+                </View>
               </View>
 
             </View>
 
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Cuaca = {this.state.forecast.main} {"\n"}
-                </Text>
-              </View>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Desc = {this.state.forecast.description} {"\n"}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Sunrise = {this.state.forecast.sunrise} {"\n"}
-                </Text>
-              </View>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Sunset = {this.state.forecast.sunset} {"\n"}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-              <Text style = {{padding: 10, fontSize: 15}} >
-                Temp = {this.state.forecast.temp} {"'Celcius"}
-              </Text>
-              </View>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Wind Speed = {this.state.forecast.speed} {"\n"}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Sea_level = {this.state.forecast.sea_level} {"\n"}
-                </Text>
-              </View>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Grnd_level = {this.state.forecast.grnd_level} {"\n"}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.backkeyboard}>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Pressure = {this.state.forecast.pressure} {"\n"}
-                </Text>
-              </View>
-              <View style={styles.box4}>
-                <Text style = {{padding: 10, fontSize: 15}} >
-                  Humidity = {this.state.forecast.grnd_level} {"\n"}
-                </Text>
-              </View>
-            </View>
-
-          </View>
-
-          <View style={styles.footer}>
-            <Text style={styles.textfooter}>Gede_Sudarma_Yasa @React_Native</Text>
+          <View style={styles.box6}>
+            <Text style={styles.text1}>Gede_Sudarma_Yasa React_Native</Text>
           </View>
         </View>
       );
     }
-}
-
-const styles = StyleSheet.create({
-  containerMain: {
-    backgroundColor: '#E8F5E9',
-    flex: 1,
-    flexDirection: 'column',
-  },
-
-  header: {
-    backgroundColor: '#1B5E20',
-    flex: 1,
-    justifyContent: 'center'
-  },
-
-  inputtext: {
-    backgroundColor: '#388E3C',
-    flex: 2,
-    justifyContent: 'center',
-    margin:10
-  },
-
-  infocuaca: {
-    backgroundColor: '#E8F5E9',
-    flex: 4,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    // alignItems: 'center',
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10
-  },
-
-  box4: {//box detail tampil
-    backgroundColor: '#A5D6A7',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    marginTop:10,
-
-    marginLeft: 10,
-    marginRight: 10
-  },
-
-  footer: {
-    backgroundColor: '#1B5E20',
-    flex: 1,
-    justifyContent: 'center'
-  },
-
-  backkeyboard: {
-    flex: 2,
-    backgroundColor: '#E8F5E9',
-    flexDirection: 'row',
-  },
-
-  text: {
-    padding: 30, fontSize: 20, color: 'white', textAlign: 'center'
-  },
-  text1: {
-    padding: 15, fontSize: 30, color: 'white', textAlign: 'center', fontWeight:'bold'
-  },
-  text2: {
-    padding: 15, fontSize: 20, color: 'white', textAlign: 'center', fontWeight:'bold'
-  },
-  textfooter: {
-    padding: 15, fontSize: 15, color: 'white', textAlign: 'center', fontWeight:'bold'
   }
-});
+
+  const styles = StyleSheet.create({
+    containerMain: {
+      backgroundColor: '#E8F5E9',
+      flex: 1,
+      flexDirection: 'column',
+    },
+
+    box1: {
+      backgroundColor: '#1B5E20',
+      flex: 1,
+      justifyContent: 'center'
+    },
+
+    box2: {
+      backgroundColor: '#388E3C',
+      flex: 1,
+      justifyContent: 'center'
+    },
+
+    box3: {
+      backgroundColor: '#E8F5E9',
+      flex: 4,
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      // alignItems: 'center',
+      marginTop: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginBottom: 10
+    },
+
+    box4: {
+      backgroundColor: 'black',
+      flexDirection: 'row',
+      flex: 2,
+    },
+
+    box5: {
+      backgroundColor: '#c6cbd1',
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      marginTop: 10,
+      marginLeft: 10,
+      marginRight: 10
+    },
+
+    box6: {
+      backgroundColor: '#4a9186',
+      flex: 1,
+      justifyContent: 'center'
+    },
+
+    text: {
+      padding: 30,
+      fontSize: 20,
+      // color: 'white',
+      // textAlign: 'center'
+    },
+
+    text1: {
+      padding: 30,
+      fontSize: 20,
+      color: 'white',
+      textAlign: 'center',
+      fontWeight: 'bold'
+    },
+
+    text2: {
+      padding: 10,
+      fontSize: 15
+    }
+  });
